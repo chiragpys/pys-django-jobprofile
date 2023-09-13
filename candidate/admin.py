e58e7cd5d14090a5ce36c59dc0cf6d7130cf70f3
“chirag” <chirag.pysquad@gmail.com>
2023-09-13 17:20:17 +0530

    I have change in admin file

--- candidate/admin.py
+++ candidate/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from .models import CandidateProfile, StudyDetail, ExperienceDetail, SkillDetail
 
+
 # Register your models here.
-admin.site.register(CandidateProfile)
+
+class StudyInline(admin.StackedInline):
+    model = StudyDetail
+    extra = 1
+
+
+class ExperienceInline(admin.StackedInline):
+    model = ExperienceDetail
+    extra = 1
+
+
+class SkillInline(admin.StackedInline):
+    model = SkillDetail
+    extra = 1
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    inlines = [StudyInline, ExperienceInline, SkillInline]
+    search_fields = ('name',)
+    list_filter = ('profile_status',)
+
+
+admin.site.register(CandidateProfile, CandidateAdmin)
 admin.site.register(StudyDetail)
 admin.site.register(ExperienceDetail)
 admin.site.register(SkillDetail)
--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import User, Manager, Agent
 
-# Register your models here.
 
-admin.site.register(User)
-admin.site.register(Manager)
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'is_manager', 'is_agent', 'date_joined')
+
+
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+# admin.site.register(User)
+# admin.site.register(Manager)
 admin.site.register(Agent)
